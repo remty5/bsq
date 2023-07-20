441d9f47c7bc46ddb792c259a625b71e03b048dd
Clement Jouenne <cjouenne@c1r3p11.42lehavre.fr>
2023-07-20 15:02:50 +0200

    ft.c

---
+++ include/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:54:53 by cjouenne          #+#    #+#             */
+/*   Updated: 2023/07/20 14:56:23 by cjouenne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int n);
+int		ft_atoi(char *str);
+
+#endif
--- src/parser.c
+++ src/parser.c
@@ -6,12 +6,13 @@
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:44:56 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/07/20 14:44:26 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/07/20 14:54:25 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "bsq.h"
@@ -23,15 +24,19 @@ void	ft_read_map(char *path)
 	char	buf[SIZE];
 	int		fd;
 	int		c;
+	int		line_count;
 
 	fd = open(path, O_RDONLY);
-	c = 0;
+	c = 1;
+	line_count = 0;
 	if (fd)
 	{
 		while (c > 0)
 		{
 			c = read(fd, buf, SIZE);
 			write(1, buf, c);
+		//	line_count = atoi(&buf[0]);
+		//	printf("line_count = %d\n", line_count);
 		}
 		if (!c)
 			exit(0);
