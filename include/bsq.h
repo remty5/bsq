631cca00459c118d5b4e7894845785ba399f414c
Clement Jouenne <cjouenne@c1r3p11.42lehavre.fr>
2023-07-20 16:00:06 +0200

    parsing number line

--- include/bsq.h
+++ include/bsq.h
@@ -6,7 +6,7 @@
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:07:02 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/07/20 14:46:40 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/07/20 15:55:44 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 void	bsq_main(int argc, char *argv[]);
 void	ft_read_map(char *path);
+int		ft_parse_first_line(char *buf);
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:44:56 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/07/20 14:54:25 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/07/20 15:59:22 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "ft.h"
 #include "bsq.h"
 
 #define SIZE 1024
@@ -31,14 +32,34 @@ void	ft_read_map(char *path)
 	line_count = 0;
 	if (fd)
 	{
-		while (c > 0)
 		{
 			c = read(fd, buf, SIZE);
-			write(1, buf, c);
-		//	line_count = atoi(&buf[0]);
-		//	printf("line_count = %d\n", line_count);
+			line_count = ft_parse_first_line(buf);
+			printf("line_count = %d\n", line_count);
 		}
 		if (!c)
 			exit(0);
 	}
+}
+
+int	ft_parse_first_line(char *buf)
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = -1;
+	j = 0;
+	res = 0;
+	while (buf[++i] != '\n')
+		;
+	i -= 4;
+	while (j <= i)
+	{
+		res += (int)(buf[j] - 48);
+		res *= 10;
+		j++;
+	}
+	res /= 10;
+	return (res);
 }
